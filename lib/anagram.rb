4ed0fbb49f33a2ecb7aed9e48d02eac36d1718ef
blackwell <blackwellsmithiv@gmail.com>
2019-02-06 23:02:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require "pry"
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  def match(words_array) #match detects no match and anagrams
+  
+    words_array.select do |a|
+      @word.split("").sort == a.split("").sort
+    
+    end
+  end
+  
+end
