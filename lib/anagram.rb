a4346a3b43f34815ae5feb9405a83ac03b0b97c8
Derek Cerretani <derekcerretani@gmail.com>
2019-02-07 22:04:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    #iterate over the array of words
+    #split each word in array into it's own array
+    #sort the word array and initalize word
+    #compare each word of that array to the word initialized
+    #return the anagrams
+    array.find_all { |anagram| anagram.split(//).sort == @word.split(//).sort }
+  end
+
+end
