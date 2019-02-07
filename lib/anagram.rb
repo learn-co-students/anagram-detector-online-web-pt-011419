2b247fd4b49ceab55ae23b98a7c8ad61ddc7bf31
A Glazer <avigayilglazer@gmail.com>
2019-02-07 17:01:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match
+    @word.match do |x|
+      x.sort == @word.sort
+binding.pry
+    end
+
+
+  end
+end
