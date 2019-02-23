e57319562bf48d2322293be3f7ea802d72f61016
Kelsey Low <kklowlow@gmail.com>
2019-02-23 03:41:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,13 @@
-class Anagram
+class Anagram 
   attr_accessor :word
-
+  
   def initialize(word)
     @word = word
   end
 
   def match(array)
-    array.select do |word|
-      word.split("").sort == @word.split("").sort
+    array.select do |w| # takes an array of possible anagrams
+      w.split("").sort == @word.split("").sort # returns all matches in an # array; if no matches exist, return an empty array
     end
   end
 end
